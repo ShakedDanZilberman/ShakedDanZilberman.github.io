45d456e5959af14082c419953a3892f1397ad851
ShakedDanZilberman <shaked.zilberman.45@gmail.com>
2025-09-05 02:36:26 +0300

    deployment of the academic videos archive v0

---
+++ AcademicVideosArchive.js
@@ -0,0 +1,63 @@
+const academicVideosArchive = {
+    "פיזיקה": [
+        {
+            "course_name": "חשמל ומגנטיות",
+            "videos": [
+                {
+                    "title": "The 4 Maxwell Equations",
+                    "url": "https://youtu.be/hJD8ywGrXks?si=ZxpEZjymqvSd1QKi"
+                }
+            ]
+        },
+        {
+            "course_name": "חשנ\"ל",
+            "videos": [
+                {
+                    "title": "Spacetime rotations, understanding Lorentz transformations",
+                    "url": "https://youtu.be/qdycfWfAtsM?si=7fKuDHrQZYyKt_yJ"
+                }
+            ]
+        }
+    ],
+    "מתמטיקה": [
+        {
+            "course_name": "אינפי",
+            "videos": [
+                {
+                    "title": "Yes, you can do induction on the real numbers!",
+                    "url": "https://youtu.be/mNJhQ36h70s?si=hsOb2YUnby17yNLw"
+                }
+            ]
+        },
+        {
+            "course_name": "חבורות",
+            "videos": [
+                {
+                    "title": "An Introduction to Group Theory",
+                    "url": "https://youtu.be/KftKCG2lD60?si=IrSXH65poX48Bg1T"
+                }
+            ]
+        },
+        {
+            "course_name": "מת\"פ",
+            "videos": [
+                {
+                    "title": "Analytic Continuation and the Zeta Function",
+                    "url": "https://youtu.be/CjSKmcWRFzE?si=ya5vg_QdhBM886JV"
+                }
+            ]
+        }
+    ],
+    "מדמ\"ח": [],
+    "תלפיות": [
+        {
+            "course_name": "טכנו\"צ 1",
+            "videos": [
+                {
+                    "title": "Phased Arrays - Steering and the Antenna Pattern",
+                    "url": "https://youtu.be/jSDLfcNhThw?si=b9vJONUy4lwWJPJH"
+                }
+            ]
+        }
+    ]
+};

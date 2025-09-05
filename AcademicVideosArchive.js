5e2953e997bf20c7aa1f3945a04f9e1587c00974
ShakedDanZilberman <shaked.zilberman.45@gmail.com>
2025-09-05 18:14:47 +0300

    stable build

--- AcademicVideosArchive.js
+++ AcademicVideosArchive.js
@@ -3,6 +3,10 @@ const redWords = ["(Extra)"];
 const academicVideosArchive = {
     "פיזיקה": [
         {
+            "course_name": "מכניקה ויחסות",
+            "videos": ["https://youtu.be/bI-FS7aZJpY?si=eq_e0O1GBli2nYpp", "https://youtu.be/bmGqhM-tUk4?si=Rs2HTtNdqFFVaN_Y", "https://youtu.be/au0QJYISe4c?si=7N7d0ic-W3HGHAcx"]
+        },
+        {
             "course_name": "חשמל ומגנטיות",
             "videos": [
                 {
@@ -12,13 +16,21 @@ const academicVideosArchive = {
             ]
         },
         {
+            "course_name": "מכנ\"ל",
+            "videos": ["https://youtu.be/hucEIa5SbXU?si=UhlO-ls3jeZ-L3Wd"]
+        },
+        {
             "course_name": "חשנ\"ל",
             "videos": ["https://youtu.be/qdycfWfAtsM?si=7fKuDHrQZYyKt_yJ"]
+        },
+        {
+            "course_name": "קוונטים 1",
+            "videos": ["https://youtu.be/RCIz2hdJQy0?si=9d3x1fhRROdketiA"]
         }
     ],
     "מתמטיקה": [
         {
-            "course_name": "אינפי",
+            "course_name": "אינפי 1",
             "videos": [
                 {
                     "title": "Induction on the Reals, and Weierstrass Extreme Value Theorem (Extra)",
@@ -42,11 +54,17 @@ const academicVideosArchive = {
                 {
                     "title": "Analytic Continuation and the Zeta Function (Extra)",
                     "url": "https://youtu.be/CjSKmcWRFzE?si=ya5vg_QdhBM886JV"
-                }
+                },
+                "https://youtu.be/ubVFzZNEphs?si=j0xHT3suk3EE0nxq"
             ]
         }
     ],
-    "מדמ\"ח": [],
+    "מדמ\"ח": [
+        {
+            "course_name": "חישוביות",
+            "videos": ["https://youtu.be/92WHN-pAFCs?si=7HwZRZfEZRWhcRDL"]
+        }
+    ],
     "תלפיות": [
         {
             "course_name": "טכנו\"צ 1",
